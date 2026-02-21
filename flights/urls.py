d015814741d1a65aa5fefb7f71a7eca3429cfaf9
Sara Salah <sarasalahmohamed97@gmail.com>
2026-02-21 22:44:48 +0200

    drop down

--- flights/urls.py
+++ flights/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search, name='search'),
+    path('airport-search/', views.airport_search, name='airport_search'),
 ]
